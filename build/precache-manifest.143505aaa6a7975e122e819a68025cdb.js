4785bc7e62e691afc3508fe492f9bfc6650913eb
junsuhlee <junsuhlee94@gmail.com>
2019-09-07 16:40:13 -0700

    push build

---
+++ build/precache-manifest.143505aaa6a7975e122e819a68025cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4da94b4089a20d53096c7c72d727e038",
+    "url": "/jasper-web/index.html"
+  },
+  {
+    "revision": "b6d2955a3fa8d31f58cf",
+    "url": "/jasper-web/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e56cf5e52f4b1e08fda3",
+    "url": "/jasper-web/static/js/2.41efbd85.chunk.js"
+  },
+  {
+    "revision": "b6d2955a3fa8d31f58cf",
+    "url": "/jasper-web/static/js/main.4382c556.chunk.js"
+  },
+  {
+    "revision": "62bb0e39f317683ccf27",
+    "url": "/jasper-web/static/js/runtime~main.309bfa4f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jasper-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
