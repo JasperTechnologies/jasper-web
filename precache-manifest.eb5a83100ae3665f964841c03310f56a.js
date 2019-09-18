7804f440b2265904803c908dc19fe47b86ec3803
15chrjef <15chrjef@gmail.com>
2019-09-17 21:50:18 -0700

    Updates

---
+++ precache-manifest.eb5a83100ae3665f964841c03310f56a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7589fb9766bb2c996baf560ea6d4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24b9fbbf2e95adbae227",
+    "url": "/static/css/main.90008055.chunk.css"
+  },
+  {
+    "revision": "632de01ac4ef714cbcc1",
+    "url": "/static/js/2.5cd96270.chunk.js"
+  },
+  {
+    "revision": "24b9fbbf2e95adbae227",
+    "url": "/static/js/main.a1aded59.chunk.js"
+  },
+  {
+    "revision": "934bc2b26ec4bee8b895",
+    "url": "/static/js/runtime~main.285721cb.js"
+  },
+  {
+    "revision": "9009dc40c63f9dafce85fb0bc5cb0742",
+    "url": "/static/media/ipadcomp.9009dc40.png"
+  },
+  {
+    "revision": "9783219a87b57a670fbe9011615b8dcc",
+    "url": "/static/media/ipadstand.9783219a.jpg"
+  },
+  {
+    "revision": "2b8098fac1548ff6834a9b66af9b0f47",
+    "url": "/static/media/logo.2b8098fa.png"
+  }
+]);
