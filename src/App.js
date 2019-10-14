323e0cdc06d2537116151f0e3cb5b3cadf72666d
15chrjef <15chrjef@gmail.com>
2019-10-14 06:31:02 -0700

    replace jasper technologies w/ jasper solutions

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit">
-        Jasper Technologies
+        Jasper Solutions
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
